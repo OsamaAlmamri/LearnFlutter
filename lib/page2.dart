289865fed84126de01d7a907ff47ff0a3135f1f3
Osama Al-mamari <yemencoder@gmail.com>
2023-01-23 21:17:14 +0300

    snackBar

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 import 'MyDrawer.dart';
 
-class Page2 extends StatelessWidget {
-const Page2({ Key? key }) : super(key: key);
+class Page2 extends StatefulWidget {
+  const Page2({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("page 2"),
-        ),
-  
-        drawer:MyDrawer(),
-     
-        body:      ListView(scrollDirection: Axis.vertical, children: [
-      Column(
-        children: [
-          Center(child: Text("page 2"))
+  State<Page2> createState() => _Page2State();
+}
 
-      ])],),
-      
+class _Page2State extends State<Page2> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("page 2"),
+      ),
+      drawer: MyDrawer(),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Column(children: [
+            Center(child: Text("alerts")),
+            ElevatedButton(
+                onPressed: () {
+                  final snackBar = SnackBar(
+                    content: const Text('Hi this is message '),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () {
+                        // Some code to undo the change.
+                      },
+                    ),
+                  );
+
+                  // Find the ScaffoldMessenger in the widget tree
+                  // and use it to show a SnackBar.
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                child: Text("show snck bar")),
+          ])
+        ],
+      ),
     );
   }
 }
