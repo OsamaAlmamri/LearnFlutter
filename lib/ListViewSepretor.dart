985ea541f4f3c2912dc8339f047f5fa40ed796d8
Osama Al-mamari <yemencoder@gmail.com>
2023-01-24 21:59:00 +0300

    SearchDelegate

--- lib/ListViewSepretor.dart
+++ lib/ListViewSepretor.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'MyDrawer.dart';
 
-class ListViewSepretor extends StatelessWidget {
+class ListViewSepretor extends StatefulWidget {
+  @override
+  State<ListViewSepretor> createState() => _ListViewSepretorState();
+}
+
+class _ListViewSepretorState extends State<ListViewSepretor> {
   List test = [
     {
       "id": 1,
@@ -202,9 +207,16 @@ class ListViewSepretor extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text("page 4"),
+          actions: [
+            IconButton(onPressed: (){
+
+              showSearch(context: context, delegate: SearchData());
+            }, icon: Icon(Icons.search))
+          ],
         ),
         drawer: MyDrawer(),
-        body: ListView.separated(
+        body:
+        ListView.separated(
             itemCount: test.length,
             separatorBuilder: (context, index) {
               return Divider(
@@ -222,4 +234,247 @@ class ListViewSepretor extends StatelessWidget {
               );
             }));
   }
+}
+
+
+class SearchData extends SearchDelegate {
+  List test = [
+    {
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female",
+      "ip_address": "26.58.193.2"
+    },
+    {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male",
+      "ip_address": "229.179.4.212"
+    },
+    {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female",
+      "ip_address": "180.66.162.255"
+    },
+    {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male",
+      "ip_address": "67.76.188.26"
+    },
+    {
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female",
+      "ip_address": "26.58.193.2"
+    },
+    {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male",
+      "ip_address": "229.179.4.212"
+    },
+    {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female",
+      "ip_address": "180.66.162.255"
+    },
+    {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male",
+      "ip_address": "67.76.188.26"
+    },
+    {
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female",
+      "ip_address": "26.58.193.2"
+    },
+    {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male",
+      "ip_address": "229.179.4.212"
+    },
+    {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female",
+      "ip_address": "180.66.162.255"
+    },
+    {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male",
+      "ip_address": "67.76.188.26"
+    },
+    {
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female",
+      "ip_address": "26.58.193.2"
+    },
+    {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male",
+      "ip_address": "229.179.4.212"
+    },
+    {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female",
+      "ip_address": "180.66.162.255"
+    },
+    {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male",
+      "ip_address": "67.76.188.26"
+    },
+    {
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female",
+      "ip_address": "26.58.193.2"
+    },
+    {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male",
+      "ip_address": "229.179.4.212"
+    },
+    {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female",
+      "ip_address": "180.66.162.255"
+    },
+    {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male",
+      "ip_address": "67.76.188.26"
+    },
+    {
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female",
+      "ip_address": "26.58.193.2"
+    },
+    {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male",
+      "ip_address": "229.179.4.212"
+    },
+    {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female",
+      "ip_address": "180.66.162.255"
+    },
+    {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male",
+      "ip_address": "67.76.188.26"
+    },
+  ];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(onPressed: (){ Navigator.pop(context);}, icon: Icon(Icons.close)),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return
+      IconButton(onPressed: (){
+        Navigator.pop(context);
+      }, icon: Icon(Icons.arrow_back));
+
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+
+    List filterData=test.where((element) => element["first_name"].toString().contains(query)).toList();
+    return   ListView.separated(
+        itemCount: filterData.length,
+        separatorBuilder: (context, index) {
+          return Divider(
+            color: Colors.lime,
+            height: 5,
+            thickness: 2,
+          );
+        },
+        itemBuilder: (context, i) {
+          return ListTile(
+            title: Text("${filterData[i]['first_name']}"),
+            subtitle: Text("${filterData[i]['email']}"),
+            trailing: Text("${filterData[i]['gender']}"),
+            leading: Text("${filterData[i]['id']}"),
+          );
+        });
+  }
+  
 }
