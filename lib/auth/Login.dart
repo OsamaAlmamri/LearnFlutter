13c56756cf1faeb22db4bb4da9bf7a25d7ec991d
Osama Al-mamari <yemencoder@gmail.com>
2023-01-24 22:27:01 +0300

    Direction rtl

--- lib/auth/Login.dart
+++ lib/auth/Login.dart
@@ -10,62 +10,65 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("تسجيل الدخول"),
-      ),
-      body:ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            Form(
-
-        child: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 20),
-                child:   CircleAvatar(
-                  radius:50,
-                  // child: Text("Osama"),
-                  backgroundColor: Colors.blue,
-                  backgroundImage: AssetImage('images/osama.jpg'),
-                ),
-                // decoration: BoxDecoration,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    label: Text("اسم المستخدم"),
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(borderSide: BorderSide(width: 2))
-                ),
-              ),
-              SizedBox(height: 20,),
-              TextFormField(
-
-                obscureText: true,
-                decoration: InputDecoration(
-
-                    label: Text("كلمة السر"),
-                    prefixIcon: Icon(Icons.remove_red_eye),
-                    border: OutlineInputBorder(borderSide: BorderSide(width: 2))
-                ),
-              ),
-              SizedBox(height: 10,),
-              Container(child: Row(children: [
-                Text("ليس لديك حساب من قبل "),
-                TextButton(onPressed: (){
-                  Navigator.of(context).pushReplacementNamed('sign_in');
-                }, child: Text("انشاء حساب"))
-              ],),),
-
-              ElevatedButton(onPressed: (){}, child: Text("تسجيل الدخول "))
-            ],
-          ),
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("تسجيل الدخول"),
         ),
-      ),])
+        body:ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              Form(
+
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child:   CircleAvatar(
+                    radius:50,
+                    // child: Text("Osama"),
+                    backgroundColor: Colors.blue,
+                    backgroundImage: AssetImage('images/osama.jpg'),
+                  ),
+                  // decoration: BoxDecoration,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      label: Text("اسم المستخدم"),
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+
+                  obscureText: true,
+                  decoration: InputDecoration(
+
+                      label: Text("كلمة السر"),
+                      prefixIcon: Icon(Icons.remove_red_eye),
+                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Container(child: Row(children: [
+                  Text("ليس لديك حساب من قبل "),
+                  TextButton(onPressed: (){
+                    Navigator.of(context).pushReplacementNamed('sign_in');
+                  }, child: Text("انشاء حساب"))
+                ],),),
+
+                ElevatedButton(onPressed: (){}, child: Text("تسجيل الدخول "))
+              ],
+            ),
+          ),
+        ),])
+      ),
     );
   }
 }
--- lib/auth/SignIn.dart
+++ lib/auth/SignIn.dart
@@ -10,72 +10,78 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("تسجيل حساب جديد"),
-      ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-       Form(
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("تسجيل حساب جديد"),
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+         Form(
 
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Column(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
 
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  child:   CircleAvatar(
-                    radius:50,
-                    // child: Text("Osama"),
-                    backgroundColor: Colors.blue,
-                    backgroundImage: AssetImage('images/osama.jpg'),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child:   CircleAvatar(
+                      radius:50,
+                      // child: Text("Osama"),
+                      backgroundColor: Colors.blue,
+                      backgroundImage: AssetImage('images/osama.jpg'),
+                    ),
+                    // decoration: BoxDecoration,
                   ),
-                  // decoration: BoxDecoration,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      label: Text("اسم المستخدم"),
-                      prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                  TextFormField(
+                    decoration: InputDecoration(
+                        label: Text("اسم المستخدم"),
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                    ),
                   ),
-                ),
-                SizedBox(height: 20,),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      label: Text(" الايميل"),
-                      prefixIcon: Icon(Icons.email),
-                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.left,
+                    textDirection: TextDirection.ltr,
+                    decoration: InputDecoration(
+                        label: Text(" الايميل"),
+                        prefixIcon: Icon(Icons.email),
+
+                        border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                    ),
                   ),
-                ),
-                SizedBox(height: 20,),
-                TextFormField(
+                  SizedBox(height: 20,),
+                  TextFormField(
 
-                  obscureText: true,
-                  decoration: InputDecoration(
+                    obscureText: true,
+                    decoration: InputDecoration(
 
-                      label: Text("كلمة السر"),
-                      prefixIcon: Icon(Icons.remove_red_eye),
-                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                        label: Text("كلمة السر"),
+                        prefixIcon: Icon(Icons.remove_red_eye),
+                        border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                    ),
                   ),
-                ),
-                SizedBox(height: 10,),
-                Container(child: Row(children: [
-                  Text("هل لديك حساب من قبل "),
-                  TextButton(onPressed: (){
+                  SizedBox(height: 10,),
+                  Container(child: Row(children: [
+                    Text("هل لديك حساب من قبل "),
+                    TextButton(onPressed: (){
 
-                    Navigator.of(context).pushReplacementNamed('login');
-                  }, child: Text("  تسجيل الدخول "))
-                ],),),
+                      Navigator.of(context).pushReplacementNamed('login');
+                    }, child: Text("  تسجيل الدخول "))
+                  ],),),
 
-                ElevatedButton(onPressed: (){}, child: Text(" انشاء حساب "))
-              ],
+                  ElevatedButton(onPressed: (){}, child: Text(" انشاء حساب "))
+                ],
+              ),
             ),
-          ),
-        ),],
+          ),],
+        ),
       ),
     );
   }
