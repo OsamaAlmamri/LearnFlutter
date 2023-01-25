fa070eee61b369c67559b244be4fcda729966a62
Osama Al-mamari <yemencoder@gmail.com>
2023-01-26 00:15:56 +0300

    FutureBuilder

--- lib/page3.dart
+++ lib/page3.dart
@@ -12,7 +12,6 @@ class Page3 extends StatefulWidget {
 }
 
 class _Page3State extends State<Page3> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -20,12 +19,12 @@ class _Page3State extends State<Page3> {
     super.initState();
   }
 
-   List test =[];
-  void get_date(List<String> arguments) async {
+  List test = [];
+
+  Future get_date(List<String> arguments) async {
     // This example uses the Google Books API to search for books about http.
     // https://developers.google.com/books/docs/overview
-    var url =
-    Uri.https('shopadmin.erum.ae', '/products', {'q': '{https}'});
+    var url = Uri.https('shopadmin.erum.ae', '/products', {'q': '{https}'});
 
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
@@ -33,44 +32,38 @@ class _Page3State extends State<Page3> {
       print("response.body");
       print(response.body);
       var jsonResponse =
-      convert.jsonDecode(response.body) as Map<String, dynamic>;
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
       var itemCount = jsonResponse['totalItems'];
-      print(jsonResponse['data']);
-      setState(() {
-        test=jsonResponse['data'];
-      });
-      print('Number of books about http: $itemCount.');
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
+
+      return jsonResponse['data'];
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("page 3"),
-        ),
-        drawer: MyDrawer(),
-        body: Column(
-          children: [
-            ElevatedButton(onPressed: (){
-              get_date([]);
-            }, child: Text("get Data")),
-            Container(
-              height: 700,
-              child:(test.length==0)?CircularProgressIndicator() : ListView.builder(
-                  itemCount: test.length,
-                  itemBuilder: (context, i) {
-                    return ListTile(
-                      title: Text("${test[i]['name']['ar']}"),
-                      subtitle: Text("${test[i]['description']['ar']}"),
-                      // trailing: Text("${test[i]['id']}"),
-                      leading: Image.network("https://shopadmin.erum.ae${test[i]['image']}",fit: BoxFit.cover,),
-                    );
-                  }),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text("page 3"),
+      ),
+      drawer: MyDrawer(),
+      body: FutureBuilder(
+        future: get_date([]),
+        builder: (context,snapshat){
+          return  (snapshat.data==null)? CircularProgressIndicator():  ListView.builder(
+              itemCount: snapshat.data.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text("${snapshat.data[i]['name']['ar']}"),
+                  subtitle: Text("${snapshat.data[i]['description']['ar']}"),
+                  // trailing: Text("${test[i]['id']}"),
+                  leading: Image.network(
+                    "https://shopadmin.erum.ae${snapshat.data[i]['image']}",
+                    fit: BoxFit.cover,
+                  ),
+                );
+              });
+        },
+      ),
+    );
   }
 }
