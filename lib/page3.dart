978e6fee2658bf5e95fc2e9c6fd9521493f38634
Osama Al-mamari <yemencoder@gmail.com>
2023-01-26 00:04:35 +0300

    http

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,201 +1,49 @@
 import 'package:flutter/material.dart';
 import 'MyDrawer.dart';
+import 'dart:convert' as convert;
 
-class Page3 extends StatelessWidget {
-     Page3({ Key? key} ) : super(key: key);
+import 'package:http/http.dart' as http;
 
-final  List test = [
-    {
-      "id": 1,
-      "first_name": "Jeanette",
-      "last_name": "Penddreth",
-      "email": "jpenddreth0@census.gov",
-      "gender": "Female",
-      "ip_address": "26.58.193.2"
-    },
-    {
-      "id": 2,
-      "first_name": "Giavani",
-      "last_name": "Frediani",
-      "email": "gfrediani1@senate.gov",
-      "gender": "Male",
-      "ip_address": "229.179.4.212"
-    },
-    {
-      "id": 3,
-      "first_name": "Noell",
-      "last_name": "Bea",
-      "email": "nbea2@imageshack.us",
-      "gender": "Female",
-      "ip_address": "180.66.162.255"
-    },
-    {
-      "id": 4,
-      "first_name": "Willard",
-      "last_name": "Valek",
-      "email": "wvalek3@vk.com",
-      "gender": "Male",
-      "ip_address": "67.76.188.26"
-    },
-    {
-      "id": 1,
-      "first_name": "Jeanette",
-      "last_name": "Penddreth",
-      "email": "jpenddreth0@census.gov",
-      "gender": "Female",
-      "ip_address": "26.58.193.2"
-    },
-    {
-      "id": 2,
-      "first_name": "Giavani",
-      "last_name": "Frediani",
-      "email": "gfrediani1@senate.gov",
-      "gender": "Male",
-      "ip_address": "229.179.4.212"
-    },
-    {
-      "id": 3,
-      "first_name": "Noell",
-      "last_name": "Bea",
-      "email": "nbea2@imageshack.us",
-      "gender": "Female",
-      "ip_address": "180.66.162.255"
-    },
-    {
-      "id": 4,
-      "first_name": "Willard",
-      "last_name": "Valek",
-      "email": "wvalek3@vk.com",
-      "gender": "Male",
-      "ip_address": "67.76.188.26"
-    },{
-      "id": 1,
-      "first_name": "Jeanette",
-      "last_name": "Penddreth",
-      "email": "jpenddreth0@census.gov",
-      "gender": "Female",
-      "ip_address": "26.58.193.2"
-    },
-    {
-      "id": 2,
-      "first_name": "Giavani",
-      "last_name": "Frediani",
-      "email": "gfrediani1@senate.gov",
-      "gender": "Male",
-      "ip_address": "229.179.4.212"
-    },
-    {
-      "id": 3,
-      "first_name": "Noell",
-      "last_name": "Bea",
-      "email": "nbea2@imageshack.us",
-      "gender": "Female",
-      "ip_address": "180.66.162.255"
-    },
-    {
-      "id": 4,
-      "first_name": "Willard",
-      "last_name": "Valek",
-      "email": "wvalek3@vk.com",
-      "gender": "Male",
-      "ip_address": "67.76.188.26"
-    },
-    {
-      "id": 1,
-      "first_name": "Jeanette",
-      "last_name": "Penddreth",
-      "email": "jpenddreth0@census.gov",
-      "gender": "Female",
-      "ip_address": "26.58.193.2"
-    },
-    {
-      "id": 2,
-      "first_name": "Giavani",
-      "last_name": "Frediani",
-      "email": "gfrediani1@senate.gov",
-      "gender": "Male",
-      "ip_address": "229.179.4.212"
-    },
-    {
-      "id": 3,
-      "first_name": "Noell",
-      "last_name": "Bea",
-      "email": "nbea2@imageshack.us",
-      "gender": "Female",
-      "ip_address": "180.66.162.255"
-    },
-    {
-      "id": 4,
-      "first_name": "Willard",
-      "last_name": "Valek",
-      "email": "wvalek3@vk.com",
-      "gender": "Male",
-      "ip_address": "67.76.188.26"
-    },{
-      "id": 1,
-      "first_name": "Jeanette",
-      "last_name": "Penddreth",
-      "email": "jpenddreth0@census.gov",
-      "gender": "Female",
-      "ip_address": "26.58.193.2"
-    },
-    {
-      "id": 2,
-      "first_name": "Giavani",
-      "last_name": "Frediani",
-      "email": "gfrediani1@senate.gov",
-      "gender": "Male",
-      "ip_address": "229.179.4.212"
-    },
-    {
-      "id": 3,
-      "first_name": "Noell",
-      "last_name": "Bea",
-      "email": "nbea2@imageshack.us",
-      "gender": "Female",
-      "ip_address": "180.66.162.255"
-    },
-    {
-      "id": 4,
-      "first_name": "Willard",
-      "last_name": "Valek",
-      "email": "wvalek3@vk.com",
-      "gender": "Male",
-      "ip_address": "67.76.188.26"
-    },
-    {
-      "id": 1,
-      "first_name": "Jeanette",
-      "last_name": "Penddreth",
-      "email": "jpenddreth0@census.gov",
-      "gender": "Female",
-      "ip_address": "26.58.193.2"
-    },
-    {
-      "id": 2,
-      "first_name": "Giavani",
-      "last_name": "Frediani",
-      "email": "gfrediani1@senate.gov",
-      "gender": "Male",
-      "ip_address": "229.179.4.212"
-    },
-    {
-      "id": 3,
-      "first_name": "Noell",
-      "last_name": "Bea",
-      "email": "nbea2@imageshack.us",
-      "gender": "Female",
-      "ip_address": "180.66.162.255"
-    },
-    {
-      "id": 4,
-      "first_name": "Willard",
-      "last_name": "Valek",
-      "email": "wvalek3@vk.com",
-      "gender": "Male",
-      "ip_address": "67.76.188.26"
-    },
-  ];
+class Page3 extends StatefulWidget {
+  Page3({Key? key}) : super(key: key);
+
+  @override
+  State<Page3> createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    get_date([]);
+    super.initState();
+  }
+
+   List test =[];
+  void get_date(List<String> arguments) async {
+    // This example uses the Google Books API to search for books about http.
+    // https://developers.google.com/books/docs/overview
+    var url =
+    Uri.https('shopadmin.erum.ae', '/products', {'q': '{https}'});
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      print("response.body");
+      print(response.body);
+      var jsonResponse =
+      convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['totalItems'];
+      print(jsonResponse['data']);
+      setState(() {
+        test=jsonResponse['data'];
+      });
+      print('Number of books about http: $itemCount.');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -204,15 +52,25 @@ final  List test = [
           title: Text("page 3"),
         ),
         drawer: MyDrawer(),
-        body: ListView.builder(
-            itemCount: test.length,
-            itemBuilder: (context, i) {
-              return ListTile(
-                title: Text("${test[i]['first_name']}"),
-                subtitle: Text("${test[i]['email']}"),
-                trailing: Text("${test[i]['gender']}"),
-                leading: Text("${test[i]['id']}"),
-              );
-            }));
+        body: Column(
+          children: [
+            ElevatedButton(onPressed: (){
+              get_date([]);
+            }, child: Text("get Data")),
+            Container(
+              height: 700,
+              child:(test.length==0)?CircularProgressIndicator() : ListView.builder(
+                  itemCount: test.length,
+                  itemBuilder: (context, i) {
+                    return ListTile(
+                      title: Text("${test[i]['name']['ar']}"),
+                      subtitle: Text("${test[i]['description']['ar']}"),
+                      // trailing: Text("${test[i]['id']}"),
+                      leading: Image.network("https://shopadmin.erum.ae${test[i]['image']}",fit: BoxFit.cover,),
+                    );
+                  }),
+            ),
+          ],
+        ));
   }
 }
