fa6afbf644cab16b3ada5af675105e8ab40b104c
Osama Al-mamari <yemencoder@gmail.com>
2023-01-25 23:14:28 +0300

    Date and time (jiffy)

--- lib/DateAndTime.dart
+++ lib/DateAndTime.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'MyDrawer.dart';
+import 'package:jiffy/jiffy.dart';
 
 class DateAndTime extends StatefulWidget {
   const DateAndTime({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class DateAndTime extends StatefulWidget {
 class _DateAndTimeState extends State<DateAndTime> {
 
 var my_time;
+var my_time_format;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +36,21 @@ var my_time;
             });
           }, child: Text("print time")),
 
-          Text("$my_time")
+          Text("$my_time"),
+          ElevatedButton(onPressed: ()async{
+            await Jiffy.locale("ar");
+            setState(() {
+              my_time=DateTime.now();
+
+             my_time_format= Jiffy().yMMMMEEEEdjm; //
+             my_time_format= Jiffy().E; //day
+             my_time_format= Jiffy().year; //day
+              // print()
+
+            });
+          }, child: Text("print my_time_format")),
+
+          Text("$my_time_format"),
         ],
       ),
     );
