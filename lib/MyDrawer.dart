ea2d6c6d1535c876062e07a8f3fcb304483c1366
Osama Al-mamari <yemencoder@gmail.com>
2023-01-26 00:43:02 +0300

    shared_preferences

--- lib/MyDrawer.dart
+++ lib/MyDrawer.dart
@@ -4,11 +4,33 @@ import 'package:my_app/Dropdown.dart';
 import 'package:my_app/main.dart';
 import 'package:my_app/page2.dart';
 import 'package:my_app/page3.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class MyDrawer extends StatelessWidget {
+class MyDrawer extends StatefulWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
   @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  String email = "";
+
+  getData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      email = prefs.getString('email')!;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getData();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
@@ -26,52 +48,179 @@ class MyDrawer extends StatelessWidget {
                       ),
                       accountName: Text("Osama Mohammed"),
                       accountEmail: Text(
-                        "yemencoder@gmail.com",
+                        email,
                       )),
                   SizedBox(
                     height: 4,
                   ),
-                  ListTile(title: Text('home '),leading:Icon(Icons.home, size: 30,color: Colors.red, ),
-                    onTap: () {Navigator.of(context).pushNamed("home");},),
-                  ListTile(title: Text('geolocator '),leading:Icon(Icons.map, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("geolocator");},),
-                  ListTile(title: Text('image_picker '),leading:Icon(Icons.camera_enhance, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("image_picker");},),
-                  ListTile(title: Text('date_and_time '),leading:Icon(Icons.timer, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("date_and_time");},),
-                  ListTile(title: Text('dropdown '),leading:Icon(Icons.arrow_drop_down, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("dropdown");},),
-                  ListTile(title: Text('dropdown_search '),leading:Icon(Icons.arrow_drop_down, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("dropdown_search");},),
-                  ListTile(title: Text('dialogs '),leading:Icon(Icons.alarm, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("page2");},),
-             ListTile(title: Text('list view buildre '),leading:Icon(Icons.list, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("page3");},),
-
-                ListTile(title: Text('list view sepretor'),leading:Icon(Icons.list_sharp, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("list_view_sepretor");},),
-
-                ListTile(title: Text('GridView'),leading:Icon(Icons.grid_3x3, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("grid_view");},),
-
-                ListTile(title: Text('tab_bar'),leading:Icon(Icons.tab, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("tab_bar");},),
-
-                ListTile(title: Text('nav_bar_btn'),leading:Icon(Icons.tab, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("nav_bar_btn");},),
-
-                ListTile(title: Text('page_view'),leading:Icon(Icons.pageview, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("page_view");},),
-
-                ListTile(title: Text('forms'),leading:Icon(Icons.input, size: 30,color: Colors.blue, ),
-                    onTap: () {Navigator.of(context).pushNamed("forms");},),
-
+                  ListTile(
+                    title: Text('home '),
+                    leading: Icon(
+                      Icons.home,
+                      size: 30,
+                      color: Colors.red,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("home");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('geolocator '),
+                    leading: Icon(
+                      Icons.map,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("geolocator");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('image_picker '),
+                    leading: Icon(
+                      Icons.camera_enhance,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("image_picker");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('date_and_time '),
+                    leading: Icon(
+                      Icons.timer,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("date_and_time");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('dropdown '),
+                    leading: Icon(
+                      Icons.arrow_drop_down,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("dropdown");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('dropdown_search '),
+                    leading: Icon(
+                      Icons.arrow_drop_down,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("dropdown_search");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('dialogs '),
+                    leading: Icon(
+                      Icons.alarm,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("page2");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('list view buildre '),
+                    leading: Icon(
+                      Icons.list,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("page3");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('list view sepretor'),
+                    leading: Icon(
+                      Icons.list_sharp,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("list_view_sepretor");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('GridView'),
+                    leading: Icon(
+                      Icons.grid_3x3,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("grid_view");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('tab_bar'),
+                    leading: Icon(
+                      Icons.tab,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("tab_bar");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('nav_bar_btn'),
+                    leading: Icon(
+                      Icons.tab,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("nav_bar_btn");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('page_view'),
+                    leading: Icon(
+                      Icons.pageview,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("page_view");
+                    },
+                  ),
+                  ListTile(
+                    title: Text('forms'),
+                    leading: Icon(
+                      Icons.input,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("forms");
+                    },
+                  ),
                 ],
               ),
-              ListTile(title: Text('back'),leading:Icon(Icons.exit_to_app, size: 30,color: Colors.blue, ),
-                onTap: () {  if (Navigator.of(context).canPop())
-                  Navigator.of(context).pop();},),
-              
+              ListTile(
+                title: Text('back'),
+                leading: Icon(
+                  Icons.exit_to_app,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                onTap: () {
+                  if (Navigator.of(context).canPop())
+                    Navigator.of(context).pop();
+                },
+              ),
             ],
           ),
         ),
--- lib/auth/Login.dart
+++ lib/auth/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -8,70 +10,92 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController email = new TextEditingController();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late Future<int> _counter;
+
+  Future<void> saveData(email)async {
+    final SharedPreferences prefs = await _prefs;
+
+    setState(() {
+      _counter = prefs.setString('email', email).then((bool success) {
+        return email;
+      });
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("تسجيل الدخول"),
-        ),
-        body:ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              Form(
-
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Column(
-
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  child:   CircleAvatar(
-                    radius:50,
-                    // child: Text("Osama"),
-                    backgroundColor: Colors.blue,
-                    backgroundImage: AssetImage('images/osama.jpg'),
-                  ),
-                  // decoration: BoxDecoration,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      label: Text("اسم المستخدم"),
-                      prefixIcon: Icon(Icons.email),
-                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
-                  ),
-                ),
-                SizedBox(height: 20,),
-                TextFormField(
-
-                  obscureText: true,
-                  decoration: InputDecoration(
-
-                      label: Text("كلمة السر"),
-                      prefixIcon: Icon(Icons.remove_red_eye),
-                      border: OutlineInputBorder(borderSide: BorderSide(width: 2))
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Container(child: Row(children: [
-                  Text("ليس لديك حساب من قبل "),
-                  TextButton(onPressed: (){
-                    Navigator.of(context).pushReplacementNamed('sign_in');
-                  }, child: Text("انشاء حساب"))
-                ],),),
-
-                ElevatedButton(onPressed: (){
-
-                  Navigator.of(context).pushReplacementNamed('home');
-                }, child: Text("تسجيل الدخول "))
-              ],
-            ),
+          appBar: AppBar(
+            title: Text("تسجيل الدخول"),
           ),
-        ),])
-      ),
+          body: ListView(scrollDirection: Axis.vertical, children: [
+            Form(
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      child: CircleAvatar(
+                        radius: 50,
+                        // child: Text("Osama"),
+                        backgroundColor: Colors.blue,
+                        backgroundImage: AssetImage('images/osama.jpg'),
+                      ),
+                      // decoration: BoxDecoration,
+                    ),
+                    TextFormField(
+                      controller: email,
+                      decoration: InputDecoration(
+                          label: Text("اسم المستخدم"),
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          label: Text("كلمة السر"),
+                          prefixIcon: Icon(Icons.remove_red_eye),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2))),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Text("ليس لديك حساب من قبل "),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pushReplacementNamed('sign_in');
+                              },
+                              child: Text("انشاء حساب"))
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () async {
+                         await saveData(email.text);
+                          Navigator.of(context).pushReplacementNamed('home');
+                        },
+                        child: Text("تسجيل الدخول "))
+                  ],
+                ),
+              ),
+            ),
+          ])),
     );
   }
 }
